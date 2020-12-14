5d9b7f55d7163d5f377e00e998e5a25b737f9014
Nicolle <ortega.nicolleclarisse@ue.edu.ph>
2020-12-14 18:15:29 +0800

    Renaming some files

--- production/index.php
+++ production/index.php
@@ -67,7 +67,7 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a href="index.html"><i class="fa fa-desktop"></i>Dashboard</a>
+                  <li><a href="index.php"><i class="fa fa-desktop"></i>Dashboard</a>
                   </li>
                   <li><a href="form_wizards.html"><i class="fa fa-edit"></i> Registration</a>
                   </li>
