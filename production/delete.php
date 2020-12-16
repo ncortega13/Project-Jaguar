b14b7dd5bea9d63831a4515bc5cdcdbcf9bf9677
Darryl Mortera <mortera.darryledison@ue.edu.ph>
2020-12-16 13:52:14 +0800

    Added the Ability to delete resident registrations

---
+++ production/delete.php
@@ -0,0 +1,22 @@
+<?php 
+$resident_id = $_POST['delete'];
+
+require 'openDB_resident.php';
+
+	$db = mysqli_connect("localhost","root","","jaguar");
+
+		$sql_u = "DELETE FROM resident WHERE resident_id = '3'";
+		$res_u = mysqli_query($db, $sql_u) or die (mysqli_error($db));
+
+		if (mysqli_num_rows($res_u) > 0) {
+  			header("Location: http://localhost/Project-Jaguar/production/residents.php");
+  			}
+		else{
+			echo '<script>
+  				alert("Error");
+					</script>';
+			echo '<script>
+				window.history.go(-2);
+					</script>';
+		}
+?>
